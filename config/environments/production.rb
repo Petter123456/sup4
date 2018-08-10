90942e3ea052525856ea5723d347b99771b3e935
Petter Fagerlund <petter.fagerlund@gmail.com>
2018-08-10 16:14:36 +0200

    added suppliers to seed file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.default_url_options = { :host => "https://radiant-plains-92986.herokuapp.com" }
 
 
   # Code is not reloaded between requests.
