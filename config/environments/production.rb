a6dbcdda160c65e70ab72c3222e9bf05d26b9149
Petter Fagerlund <petter.fagerlund@gmail.com>
2018-08-13 21:47:54 +0200

    private

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,27 +94,27 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-  config.action_mailer.delivery_method = :sendmail
-  # Defaults to:
-  # config.action_mailer.sendmail_settings = {
-  #   location: '/usr/sbin/sendmail',
-  #   arguments: '-i'
-  # }
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'petter.fagerlund@gmail.com'}
-
-# Gmail mailer . reference launchschool
-
-
-config.action_mailer.delivery_method = :smtp
-# SMTP settings for gmail
-config.action_mailer.smtp_settings = {
- :address              => "smtp.gmail.com",
- :port                 => 587,
- :user_name            => ENV["GMAIL_USERNAME"],
- :password             => ENV["GMAIL_PASSWORD"],
- :authentication       => "plain",
-:enable_starttls_auto => true
-}
+#   config.action_mailer.delivery_method = :sendmail
+#   # Defaults to:
+#   # config.action_mailer.sendmail_settings = {
+#   #   location: '/usr/sbin/sendmail',
+#   #   arguments: '-i'
+#   # }
+#   config.action_mailer.perform_deliveries = true
+#   config.action_mailer.raise_delivery_errors = true
+#   config.action_mailer.default_options = {from: 'petter.fagerlund@gmail.com'}
+#
+# # Gmail mailer . reference launchschool
+#
+#
+# config.action_mailer.delivery_method = :smtp
+# # SMTP settings for gmail
+# config.action_mailer.smtp_settings = {
+#  :address              => "smtp.gmail.com",
+#  :port                 => 587,
+#  :user_name            => ENV["GMAIL_USERNAME"],
+#  :password             => ENV["GMAIL_PASSWORD"],
+#  :authentication       => "plain",
+# :enable_starttls_auto => true
+# }
 end
