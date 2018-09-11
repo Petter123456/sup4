f933b77e8eb9d8d8fe68c9c037e2a2b69ba4fea6
Petter Fagerlund <petter.fagerlund@gmail.com>
2018-09-12 01:55:27 +0200

    private

--- app/mailers/confirmation_mailer.rb
+++ app/mailers/confirmation_mailer.rb
@@ -1,6 +1,7 @@
 class ConfirmationMailer < ApplicationMailer
   default from: "me@digirek.se"
-
+  RestClient.post "https://api:f4039bef2651e41269d28d81e65b9b26-c1fe131e-118b2137"\"@api.mailgun.net/v3/digirek.se/messages"
+  
     def confirmation_email(user) #user from controller
       @user = user
       @contract = Contract.last
