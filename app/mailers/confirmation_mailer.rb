e4d8bba5b88c4b13000c3498a09f6b7375dacd72
Petter Fagerlund <petter.fagerlund@gmail.com>
2018-09-17 23:34:09 +0200

    private

--- app/mailers/confirmation_mailer.rb
+++ app/mailers/confirmation_mailer.rb
@@ -4,6 +4,15 @@ class ConfirmationMailer < ApplicationMailer
     def confirmation_email(user) #user from controller
       @user = user
       @contract = Contract.last
+
+      mg_client = Mailgun::Client.new ENV['api_key']
+          message_params = {:from    => ENV['gmail_username'],
+                            :to      => @user.email,
+                            :subject => 'Sample Mail using Mailgun API',
+                            :text    => 'This mail is sent using Mailgun API via mailgun-ruby'}
+          mg_client.send_message ENV['domain'], message_params
+
+
       mail(to: @user.email, subject: 'Order Confirmation')
     end
 
@@ -11,6 +20,15 @@ class ConfirmationMailer < ApplicationMailer
       @user = user
       @supplier = supplier
       @contract = contract
+
+      mg_client = Mailgun::Client.new ENV['api_key']
+          message_params = {:from    => ENV['gmail_username'],
+                            :to      => @user.email,
+                            :subject => 'Sample Mail using Mailgun API',
+                            :text    => 'This mail is sent using Mailgun API via mailgun-ruby'}
+          mg_client.send_message ENV['domain'], message_params
+
+
       mail(to: @supplier.email, subject: 'New Order Incoming!')
     end
 
@@ -19,6 +37,15 @@ class ConfirmationMailer < ApplicationMailer
       @supplier = supplier
       @admin = admin
       @contract = contract
+
+      mg_client = Mailgun::Client.new ENV['api_key']
+          message_params = {:from    => ENV['gmail_username'],
+                            :to      => @user.email,
+                            :subject => 'Sample Mail using Mailgun API',
+                            :text    => 'This mail is sent using Mailgun API via mailgun-ruby'}
+          mg_client.send_message ENV['domain'], message_params
+
+
       mail(to: @admin, subject: 'NY Beställning!! =)')
     end
     #
